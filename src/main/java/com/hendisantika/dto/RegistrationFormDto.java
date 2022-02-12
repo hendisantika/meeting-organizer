6b45b394d2d74e8b3a8f8b8468efdeb47d242695
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-12 21:52:58 +0700

    #6 - Add RegistrationFormDto

---
+++ src/main/java/com/hendisantika/dto/RegistrationFormDto.java
@@ -0,0 +1,62 @@
+package com.hendisantika.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/02/22
+ * Time: 21.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@FieldsValueMatch.List({
+        @FieldsValueMatch(
+                field = "password",
+                fieldMatch = "confirmPassword",
+                message = "FieldsValueMatch.passwordsNotMatch"
+        ),
+        @FieldsValueMatch(
+                field = "email",
+                fieldMatch = "confirmEmail",
+                message = "FieldsValueMatch.emailsNotMatch"
+        )
+})
+public class RegistrationFormDto {
+
+    @NotBlank
+    @Size(min = 2)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2)
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @ValidEmail
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @ValidEmail
+    private String confirmEmail;
+
+    @NotBlank
+    @Size(min = 8)
+    @ValidPassword
+    private String password;
+
+    @NotBlank
+    @Size(min = 8)
+    @ValidPassword
+    private String confirmPassword;
+}
