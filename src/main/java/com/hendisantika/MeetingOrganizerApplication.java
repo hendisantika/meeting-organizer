8bfc284365f7d33ace266bbc3a3a033dea776964
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-12 21:37:22 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/MeetingOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingOrganizerApplication.class, args);
+    }
+
+}
