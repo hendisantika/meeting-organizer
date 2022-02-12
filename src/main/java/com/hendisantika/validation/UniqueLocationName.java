b074da83d96e481f13a95d411ef8e243d21fe852
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-12 21:54:37 +0700

    #8 - Add UniqueLocationName

---
+++ src/main/java/com/hendisantika/validation/UniqueLocationName.java
@@ -0,0 +1,54 @@
+package com.hendisantika.validation;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/02/22
+ * Time: 21.53
+ * To change this template use File | Settings | File Templates.
+ */
+@Target({ANNOTATION_TYPE, TYPE, FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueLocationName.CustomLocationNameValidator.class)
+@Documented
+public @interface UniqueLocationName {
+
+    String message() default "This location name is already taken";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    class CustomLocationNameValidator
+            implements ConstraintValidator<UniqueLocationName, String> {
+
+        private final LocationService locationService;
+
+        public CustomLocationNameValidator(LocationService locationService) {
+            this.locationService = locationService;
+        }
+
+        @Override
+        public void initialize(UniqueLocationName constraintAnnotation) {
+        }
+
+        @Override
+        public boolean isValid(String locationName, ConstraintValidatorContext context) {
+            return !locationService.existsByNameIgnoreCase(locationName);
+        }
+    }
+}
