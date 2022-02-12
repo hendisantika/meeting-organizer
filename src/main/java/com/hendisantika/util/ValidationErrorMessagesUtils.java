7be5c71cd48f4bef4ead8713f640f832b982e809
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-12 21:57:57 +0700

    #13 - Add errorMessagesForClassLevelValidations in ValidationErrorMessagesUtils

--- src/main/java/com/hendisantika/util/ValidationErrorMessagesUtils.java
+++ src/main/java/com/hendisantika/util/ValidationErrorMessagesUtils.java
@@ -3,9 +3,9 @@ package com.hendisantika.util;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.ObjectError;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +22,31 @@ public class ValidationErrorMessagesUtils implements MessageSourceAware {
 
     private static final List<String> CLASS_LEVEL_VALIDATION_ERROR_CODES = Arrays.asList("FieldsValueMatch");
     private MessageSource messageSource;
+
+    /**
+     * For given list of global validations errors, check if they are connected with class-level validations
+     * If they are, read their validation message from message bundle and add to map
+     *
+     * @param globalErrors global validation errors
+     * @return Map of entries, where key is the name of error, and value is validation message read from properties
+     */
+    public Map<String, String> errorMessagesForClassLevelValidations(List<ObjectError> globalErrors) {
+        Map<String, String> messages = new HashMap<>();
+
+        for (ObjectError globalError : globalErrors) {
+            String[] errorCodes = globalError.getCodes();
+
+            for (String errorCode : errorCodes) {
+                if (CLASS_LEVEL_VALIDATION_ERROR_CODES.contains(errorCode)) {
+                    String errorMessage = globalError.getDefaultMessage();
+                    String messageKey = extractMessageKeyNameFromErrorMessage(errorMessage);
+                    String messageValue = messageSource.getMessage(errorMessage, null, Locale.getDefault());
+                    messages.put(messageKey, messageValue);
+                }
+            }
+        }
+
+        return messages;
+    }
 
 }
