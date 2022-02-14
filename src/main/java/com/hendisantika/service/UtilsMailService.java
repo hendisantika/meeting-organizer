bb5854c122227ef1bf48368bac8ceca7475eb4b0
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-14 08:29:18 +0700

    #45 - Add UtilsMailService

---
+++ src/main/java/com/hendisantika/service/UtilsMailService.java
@@ -0,0 +1,32 @@
+package com.hendisantika.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/02/22
+ * Time: 08.28
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class UtilsMailService implements MessageSourceAware, MailService {
+
+    private final JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String mailAccount;
+    private MessageSource messageSource;
+
+    @Autowired
+    public UtilsMailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+}
