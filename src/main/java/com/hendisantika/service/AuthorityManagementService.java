f1e456693877d83628c003ae7108e23921285617
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-14 08:22:29 +0700

    #31 - Add findAuthorityByNameCreateAuthorityIfNotFound in AuthorityManagementService

--- src/main/java/com/hendisantika/service/AuthorityManagementService.java
+++ src/main/java/com/hendisantika/service/AuthorityManagementService.java
@@ -1,5 +1,6 @@
 package com.hendisantika.service;
 
+import com.hendisantika.domain.Authority;
 import com.hendisantika.repository.AuthorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,4 +20,12 @@ public class AuthorityManagementService implements AuthorityService {
 
     @Autowired
     private AuthorityRepository authorityRepository;
+
+    @Override
+    public Authority findAuthorityByNameCreateAuthorityIfNotFound(String authorityName) {
+        Authority authority = authorityRepository.findByAuthority(authorityName);
+        authority = (authority == null) ? saveAuthority(authorityName) : authority;
+
+        return authority;
+    }
 }
