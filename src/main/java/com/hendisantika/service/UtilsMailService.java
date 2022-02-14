d7f688cc5e9d4940280c117d1ec715ac309ed7c8
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-14 08:29:37 +0700

    #46 - Add prepareRegistrationMailMessage in UtilsMailService

--- src/main/java/com/hendisantika/service/UtilsMailService.java
+++ src/main/java/com/hendisantika/service/UtilsMailService.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +31,19 @@ public class UtilsMailService implements MessageSourceAware, MailService {
     @Autowired
     public UtilsMailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
+    }
+
+    @Override
+    public SimpleMailMessage prepareRegistrationMailMessage(String applicationUrl, String to, Locale locale) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        String mailText = messageSource.getMessage("registrationPage.emailMessage", new Object[]{applicationUrl}, locale);
+        String mailSubject = messageSource.getMessage("registrationPage.emailSubject", null, locale);
+
+        mailMessage.setTo(to);
+        mailMessage.setSubject(mailSubject);
+        mailMessage.setText(mailText);
+        mailMessage.setFrom(mailAccount);
+
+        return mailMessage;
     }
 }
