f829f5cc7acb08b2cff0a8bc757cce411214ec48
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-14 08:31:07 +0700

    #50 - Add TokenManagementService

---
+++ src/main/java/com/hendisantika/service/TokenManagementService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.VerificationTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/02/22
+ * Time: 08.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class TokenManagementService implements TokenService {
+
+    private final VerificationTokenRepository tokenRepository;
+
+    @Autowired
+    public TokenManagementService(VerificationTokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+}
