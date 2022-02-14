6e9336078ded5bf88623f25306e391612f008e91
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-14 08:17:57 +0700

    #23 - Add ProfileMailDto

---
+++ src/main/java/com/hendisantika/dto/profile/ProfileMailDto.java
@@ -0,0 +1,52 @@
+package com.hendisantika.dto.profile;
+
+import com.hendisantika.validation.FieldsValueMatch;
+import com.hendisantika.validation.ValidEmail;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/02/22
+ * Time: 08.17
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@FieldsValueMatch.List({
+        @FieldsValueMatch(
+                field = "email",
+                fieldMatch = "confirmEmail",
+                message = "FieldsValueMatch.emailsNotMatch"
+        )
+})
+public class ProfileMailDto {
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @ValidEmail
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @ValidEmail
+    private String confirmEmail;
+
+    public ProfileMailDto() {
+    }
+
+    @Override
+    public String toString() {
+        return "ProfileMailDto{" +
+                ", email='" + email + '\'' +
+                ", confirmEmail='" + confirmEmail + '\'' +
+                '}';
+    }
+}
