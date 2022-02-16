f548e2acf73bb11c0847a13471ca402076b9b671
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-16 10:58:15 +0700

    #81 - Add getUsersProfileImage in ImageController

--- src/main/java/com/hendisantika/controller/ImageController.java
+++ src/main/java/com/hendisantika/controller/ImageController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.domain.User;
 import com.hendisantika.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,5 +30,20 @@ public class ImageController {
     @Autowired
     public ImageController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping
+    public void getUsersProfileImage(@PathVariable long id, HttpServletResponse response) throws IOException {
+        User user = userService.findOne(id);
+        byte[] image = null;
+
+        if (user != null) {
+            image = user.getProfilePicture();
+        }
+
+        if (image != null) {
+            response.getOutputStream().write(userService.findOne(id).getProfilePicture());
+            response.setContentType("image/jpg");
+        }
     }
 }
