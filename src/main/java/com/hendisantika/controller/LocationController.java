aaf6c99c1371389cbc48d7328592cd01cce463b2
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-16 10:59:05 +0700

    #83 - Add displayLocationsPage in LocationController

--- src/main/java/com/hendisantika/controller/LocationController.java
+++ src/main/java/com/hendisantika/controller/LocationController.java
@@ -3,6 +3,7 @@ package com.hendisantika.controller;
 import com.hendisantika.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -29,5 +30,10 @@ public class LocationController {
     @Autowired
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
+    }
+
+    @GetMapping
+    public String displayLocationsPage() {
+        return LOCATIONS_PAGE;
     }
 }
