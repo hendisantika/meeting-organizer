58f0502b09582ad0f66775b249866dcf1f832333
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-19 08:44:59 +0700

    #178 - Add sendEmail_shouldCallMailSender in UtilsMailServiceTest :green_book: :pencil2: :tiger:

--- src/test/java/com/hendisantika/service/UtilsMailServiceTest.java
+++ src/test/java/com/hendisantika/service/UtilsMailServiceTest.java
@@ -1,10 +1,16 @@
 package com.hendisantika.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +33,13 @@ public class UtilsMailServiceTest {
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
+
+    @Test
+    public void sendEmail_shouldCallMailSender() {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailService.sendEmail(mailMessage);
+
+        verify(mailSender, times(1)).send(any(SimpleMailMessage.class));
+    }
+
 }
