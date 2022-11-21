c4170fc77ee83c598b230b5b0937a3ae7fa43b81
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-21 19:03:34 +0700

    #197 - Add homeController_isNotNull in HomeControllerTest :green_book: :pencil2: :tiger:

--- src/test/java/com/hendisantika/controller/HomeControllerTest.java
+++ src/test/java/com/hendisantika/controller/HomeControllerTest.java
@@ -4,6 +4,7 @@ import com.hendisantika.MeetingOrganizerApplication;
 import com.hendisantika.config.MeetingOrganizerConfiguration;
 import com.hendisantika.config.SecurityConfiguration;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,6 +12,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.Filter;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Created by IntelliJ IDEA.
@@ -44,5 +47,10 @@ public class HomeControllerTest {
                 .webAppContextSetup(wac)
                 .addFilter(springSecurityFilterChain)
                 .build();
+    }
+
+    @Test
+    public void homeController_isNotNull() {
+        assertNotNull(homeController);
     }
 }
