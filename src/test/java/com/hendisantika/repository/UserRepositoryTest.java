360de176b63522d50f39a8467c745275456333bb
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-21 18:52:56 +0700

    #184 - Add UserRepositoryTest :green_book: :pencil2: :tiger:

---
+++ src/test/java/com/hendisantika/repository/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.hendisantika.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/21/22
+ * Time: 18:52
+ * To change this template use File | Settings | File Templates.
+ */
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
