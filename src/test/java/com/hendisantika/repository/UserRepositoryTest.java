6c6a9a923071b75fc9d96f5d3e7898254508c1a5
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-21 18:55:19 +0700

    #185 - Add countAllByEmail_noRecordsFound_shouldReturn0 in UserRepositoryTest :green_book: :pencil2: :tiger:

--- src/test/java/com/hendisantika/repository/UserRepositoryTest.java
+++ src/test/java/com/hendisantika/repository/UserRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.hendisantika.repository;
 
+import com.hendisantika.domain.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +26,22 @@ public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Test
+    public void countAllByEmail_noRecordsFound_shouldReturn0() {
+        String userMail = "user@mail.com",
+                searchedMail = "other@mail.com";
+        User user = new User();
+        user.setFirstName("first");
+        user.setLastName("last");
+        user.setPassword("password");
+        user.setEmail(userMail);
+
+        testEntityManager.persistAndFlush(user);
+
+        Long usersWithSameEmail = userRepository.countAllByEmailIgnoreCase(searchedMail);
+
+        assertEquals(Long.valueOf(0), usersWithSameEmail);
+    }
 
 }
