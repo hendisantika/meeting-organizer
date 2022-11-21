52bb6ef21ab68c5e89b4c2ba4a367ad22ab532a7
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-21 19:03:13 +0700

    #196 - Add setUp in HomeControllerTest :green_book: :pencil2: :tiger:

--- src/test/java/com/hendisantika/controller/HomeControllerTest.java
+++ src/test/java/com/hendisantika/controller/HomeControllerTest.java
@@ -3,9 +3,11 @@ package com.hendisantika.controller;
 import com.hendisantika.MeetingOrganizerApplication;
 import com.hendisantika.config.MeetingOrganizerConfiguration;
 import com.hendisantika.config.SecurityConfiguration;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.Filter;
@@ -36,4 +38,11 @@ public class HomeControllerTest {
 
     private MockMvc mvc;
 
+    @BeforeEach
+    public void setUp() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .addFilter(springSecurityFilterChain)
+                .build();
+    }
 }
