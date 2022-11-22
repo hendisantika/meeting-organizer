226e7e420897ad67198c0f181f46d0e0e1935ace
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-23 06:15:18 +0700

    #213 - Add redirectToLogin in LoginController :green_book: :pencil2: :tiger:

--- src/main/java/com/hendisantika/controller/LoginController.java
+++ src/main/java/com/hendisantika/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.hendisantika.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +17,9 @@ import org.springframework.stereotype.Controller;
 public class LoginController {
 
     public static final String LOGIN_PAGE = "login";
+
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/login";
+    }
 }
