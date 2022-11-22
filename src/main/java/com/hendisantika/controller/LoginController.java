7c6036165b8e5340426e35f0b328dbbb9014125a
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-23 06:15:00 +0700

    #212 - Add LoginController :green_book: :pencil2: :tiger:

---
+++ src/main/java/com/hendisantika/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.hendisantika.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/23/22
+ * Time: 06:14
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class LoginController {
+
+    public static final String LOGIN_PAGE = "login";
+}
