881334ef8265439e4b52f9d4019283eb915686b4
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-23 06:19:56 +0700

    #220 - Add uploadProfileImage in ProfileController :green_book: :pencil2: :tiger:

--- src/main/java/com/hendisantika/controller/ProfileController.java
+++ src/main/java/com/hendisantika/controller/ProfileController.java
@@ -1,11 +1,16 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.domain.User;
 import com.hendisantika.service.UserService;
 import com.hendisantika.util.ValidationErrorMessagesUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,6 +41,31 @@ public class ProfileController {
 
     @GetMapping
     public String displayProfilePage() {
+        return PROFILE_PAGE;
+    }
+
+    @PostMapping
+    public String uploadProfileImage(Authentication authentication,
+                                     MultipartFile file,
+                                     RedirectAttributes redirectAttributes) {
+        User currentUSer = (User) authentication.getPrincipal();
+
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("fileEmptyError");
+            return REDIRECT_TO_PROFILE;
+        } else if (!file.getContentType().startsWith("image")) {
+            redirectAttributes.addFlashAttribute("fileNotImage");
+            return REDIRECT_TO_PROFILE;
+        }
+
+        try {
+            currentUSer.setProfilePicture(file.getBytes());
+            userService.saveUserAndFlush(currentUSer);
+        } catch (Exception e) {
+            return "redirect:/uploadError";
+        }
+
         return PROFILE_PAGE;
     }
 }
