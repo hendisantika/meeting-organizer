45c08091887228d61468d06fe7fda3b4629f6a3e
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-25 13:37:23 +0700

    #244 - Add setup in RegistrationControllerTest.java :green_book: :pencil2: :tiger:

--- src/test/java/com/hendisantika/controller/RegistrationControllerTest.java
+++ src/test/java/com/hendisantika/controller/RegistrationControllerTest.java
@@ -5,10 +5,12 @@ import com.hendisantika.config.MeetingOrganizerConfiguration;
 import com.hendisantika.config.SecurityConfiguration;
 import com.hendisantika.service.MailService;
 import com.hendisantika.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.Filter;
@@ -45,4 +47,12 @@ public class RegistrationControllerTest {
 
     @MockBean
     private MailService mailService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .addFilter(springSecurityFilterChain)
+                .build();
+    }
 }
