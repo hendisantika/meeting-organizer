c9ddfe3a595f3a5072601922ed39d95d0130c063
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-25 13:29:24 +0700

    #228 - Add ProfileControllerTest :green_book: :pencil2: :tiger:

---
+++ src/test/java/com/hendisantika/controller/ProfileControllerTest.java
@@ -0,0 +1,43 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.MeetingOrganizerApplication;
+import com.hendisantika.config.MeetingOrganizerConfiguration;
+import com.hendisantika.config.SecurityConfiguration;
+import com.hendisantika.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.Filter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : meeting-organizer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/25/22
+ * Time: 13:28
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest(classes = {MeetingOrganizerApplication.class, SecurityConfiguration.class, MeetingOrganizerConfiguration.class})
+public class ProfileControllerTest {
+    private static final String PROFILE_URL = "/profile";
+    private static final String EDIT_PROFILE_URL = "/profile/edit";
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    @Autowired
+    private ProfileController profileController;
+
+    @MockBean
+    private UserService userService;
+}
