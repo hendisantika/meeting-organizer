42c811de9739fb44bf64ed47d3fb7a22756f7212
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-25 13:29:49 +0700

    #229 - Add setup in ProfileControllerTest :green_book: :pencil2: :tiger:

--- src/test/java/com/hendisantika/controller/ProfileControllerTest.java
+++ src/test/java/com/hendisantika/controller/ProfileControllerTest.java
@@ -4,10 +4,12 @@ import com.hendisantika.MeetingOrganizerApplication;
 import com.hendisantika.config.MeetingOrganizerConfiguration;
 import com.hendisantika.config.SecurityConfiguration;
 import com.hendisantika.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.Filter;
@@ -40,4 +42,12 @@ public class ProfileControllerTest {
 
     @MockBean
     private UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .addFilter(springSecurityFilterChain)
+                .build();
+    }
 }
